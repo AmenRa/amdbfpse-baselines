99a76d2334aa78acfea38fd622c633e05b5b4d8d
AmenRa <elias.bssn@gmail.com>
2022-09-28 16:41:01 +0200

    Download script + README update

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # A Multi-Domain Benchmark for Personalized Search Evaluation Baselines
 Baselines from the paper A Multi-Domain Benchmark for Personalized Search Evaluation.
 
+The following guide assumes you are using a Linux distro, such as Ubuntu.
 
-## Step 1 - Create and activate the CONDA env
+## Step 1 - Download the datasets
+Install [wget](https://www.gnu.org/software/wget/):
+```sh
+sudo apt install wget
+```
+Install [7-zip](https://www.7-zip.org):
+```sh
+sudo apt install p7zip-full p7zip-rar
+```
+Download the benchmark datasets with the provided script.
+It will download, decompress, and move the datasets in the appropriate folders.
+```sh
+sh download.sh
+```
+If you want to get rid of the original archives exectute the following command:
+```sh
+rm -rf ./tmp
+```
+
+## Step 2 - Create and activate the CONDA env
 ```sh
 conda env create -f env.yml
 ```
@@ -10,7 +30,8 @@ conda env create -f env.yml
 conda activate amdbfpse-baselines
 ```
 
-## Step 2 - Download language model locally
+
+## Step 3 - Clone the language model locally
 Install [Git LFS](https://git-lfs.github.com):
 ```sh
 sudo apt-get install git-lfs
---
+++ download.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LIGHT_GREEN='\033[1;32m'
+LIGHT_BLUE='\033[1;34m'
+NO_COLOR='\033[0m'
+
+# Create temporary folder
+mkdir -p tmp
+
+# Create dataset folder
+mkdir -p datasets
+
+# Download Computer Science dataset archive
+printf "${LIGHT_BLUE}Starting Computer Science dataset archive download...\n${NO_COLOR}"
+wget -c -O tmp/computer_science.zip https://zenodo.org/record/6606557/files/computer_science.zip
+printf "${LIGHT_GREEN}Computer Science dataset archive download: DONE\n\n${NO_COLOR}"
+# Decompress Computer Science dataset archive
+printf "${LIGHT_BLUE}Starting Computer Science dataset archive extraction...\n${NO_COLOR}"
+7z x -o"datasets" tmp/computer_science.zip
+printf "${LIGHT_GREEN}Computer Science dataset archive extraction: DONE\n\n${NO_COLOR}"
+
+# Download Physics dataset archive
+printf "${LIGHT_BLUE}Starting Physics dataset archive download...\n${NO_COLOR}"
+wget -c -O tmp/physics.zip https://zenodo.org/record/6606557/files/physics.zip
+printf "${LIGHT_GREEN}Physics dataset archive download: DONE\n\n${NO_COLOR}"
+# Decompress Physics dataset archive
+printf "${LIGHT_BLUE}Starting Physics dataset archive extraction...\n${NO_COLOR}"
+7z x -o"datasets" tmp/physics.zip
+printf "${LIGHT_GREEN}Physics dataset archive extraction: DONE\n\n${NO_COLOR}"
+
+# Download Political Science dataset archive
+printf "${LIGHT_BLUE}Starting Political Science dataset archive download...\n${NO_COLOR}"
+wget -c -O tmp/poltical_science.zip https://zenodo.org/record/6606557/files/poltical_science.zip
+printf "${LIGHT_GREEN}Political Science dataset archive download: DONE\n\n${NO_COLOR}"
+# Decompress Political Science dataset archive
+printf "${LIGHT_BLUE}Starting Political Science dataset archive extraction...\n${NO_COLOR}"
+7z x -o"datasets" tmp/poltical_science.zip
+printf "${LIGHT_GREEN}Political Science dataset archive extraction: DONE\n\n${NO_COLOR}"
+
+# Download Psychology dataset archive
+printf "${LIGHT_BLUE}Starting Psychology dataset archive download...\n${NO_COLOR}"
+wget -c -O tmp/psychology.zip https://zenodo.org/record/6606557/files/psychology.zip
+printf "${LIGHT_GREEN}Psychology dataset archive download: DONE\n\n${NO_COLOR}"
+# Decompress Psychology dataset archive
+printf "${LIGHT_BLUE}Starting Psychology dataset archive extraction...\n${NO_COLOR}"
+7z x -o"datasets" tmp/psychology.zip
+printf "${LIGHT_GREEN}Psychology dataset archive extraction: DONE\n\n${NO_COLOR}"
